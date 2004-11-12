e3dc6a5f6d1a3b6b78c468d462c7f0c65f4e82ce
Ron Hitchens <ron.hitchens@marklogic.com>
2004-11-12 19:31:30 +0000

    Step one of folding the branch into the trunk. (subversion branch merging is a pain)

---
+++ src/java/com/marklogic/xqrunner/XQHelper.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c)2004 Mark Logic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The use of the Apache License does not indicate that this project is
+ * affiliated with the Apache Software Foundation.
+ */
+package com.marklogic.xqrunner;
+
+import org.w3c.dom.Document;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ron
+ * Date: Oct 18, 2004
+ * Time: 11:07:30 PM
+ */
+public class XQHelper
+{
+	private XQHelper()
+	{
+		// no no no
+	}
+
+	public static String executeString (XQRunner runner, XQuery query, String separator)
+		throws XQException
+	{
+		XQResult result = runner.runQuery (query);
+
+		return (result.asString (separator));
+	}
+
+	public static String executeString (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		return (executeString (runner, query, ""));
+	}
+
+	public static String [] executeStrings (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResult result = runner.runQuery (query);
+		String [] strings = new String [result.getSize()];
+
+		for (int i = 0; i < strings.length; i++) {
+			strings [i] = result.getItem (i).asString();
+		}
+
+		return (strings);
+	}
+
+	public static Object executeAtomic (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getAtomic0 (runner, query);
+
+		return (item.asObject());
+	}
+
+	public static Object executeObject (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getItem0 (runner, query);
+
+		return (item.asObject());
+	}
+
+	public static Object [] executeObjects (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResult result = runner.runQuery (query);
+		Object [] objects = new Object [result.getSize()];
+
+		for (int i = 0; i < objects.length; i++) {
+			objects [i] = result.getItem (i).asObject();
+		}
+
+		return (objects);
+	}
+
+	public static boolean executeBoolean (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getTypedItem0 (runner, query, XQVariableType.XS_BOOLEAN);
+
+		return (((Boolean) item.asObject()).booleanValue());
+	}
+
+	public static int executeInteger (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getTypedItem0 (runner, query, XQVariableType.XS_INTEGER);
+
+		return (((Integer) item.asObject()).intValue());
+	}
+
+	public static float executeFloat (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getTypedItem0 (runner, query, XQVariableType.XS_FLOAT);
+
+		return (((Float) item.asObject()).floatValue());
+	}
+
+	public static double executeDouble (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getTypedItem0 (runner, query, XQVariableType.XS_DOUBLE);
+
+		return (((Double) item.asObject()).doubleValue());
+	}
+
+	public static double executeDecimal (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getTypedItem0 (runner, query, XQVariableType.XS_DECIMAL);
+
+		return (((BigDecimal) item.asObject()).doubleValue());
+	}
+
+	public static org.jdom.Document executeJDom (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getNode0 (runner, query);
+
+		return (item.asJDom());
+	}
+
+	public static Document executeW3CDom (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getNode0 (runner, query);
+
+		return (item.asW3cDom());
+	}
+
+	// ----------------------------------------------------------------
+
+	private static XQResultItem getNode0 (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getItem0 (runner, query);
+
+		if ( ! item.isNode()) {
+			throw new XQException ("Result is not a node (" + item.getType() + ")");
+		}
+
+		return item;
+	}
+
+	private static XQResultItem getAtomic0 (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResultItem item = getItem0 (runner, query);
+
+		if (item.isNode()) {
+			throw new XQException ("Result is not atomic (" + item.getType() + ")");
+		}
+
+		return item;
+	}
+
+	private static XQResultItem getItem0 (XQRunner runner, XQuery query)
+		throws XQException
+	{
+		XQResult result = runner.runQuery (query);
+
+		if (result.getSize() < 1) {
+			throw new XQException ("Empty result sequence");
+		}
+
+		return (result.getItem (0));
+	}
+
+	private static XQResultItem getTypedItem0 (XQRunner runner, XQuery query,
+		XQVariableType expected)
+		throws XQException
+	{
+		XQResultItem item = getItem0 (runner, query);
+		XQVariableType actual = item.getType();
+
+		if (actual != expected) {
+			throw new XQException ("Item is not of expected type: expected=" +
+				expected + ", actual=" + actual);
+		}
+
+		return (item);
+	}
+}
---
+++ src/java/com/marklogic/xqrunner/generic/GenericDocumentWrapper.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c)2004 Mark Logic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The use of the Apache License does not indicate that this project is
+ * affiliated with the Apache Software Foundation.
+ */
+package com.marklogic.xqrunner.generic;
+
+import com.marklogic.xqrunner.XQDocumentWrapper;
+
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ron
+ * Date: Oct 20, 2004
+ * Time: 4:31:45 PM
+ */
+public class GenericDocumentWrapper implements XQDocumentWrapper
+{
+	private String body = null;
+	private InputStream inputStream = null;
+	private Reader reader = null;
+
+	private boolean xml = false;
+	private boolean text = false;
+	private boolean binary = false;
+
+	// ---------------------------------------------------------------
+
+	private GenericDocumentWrapper (String body, boolean isXml)
+	{
+		this.body = body;
+
+		this.xml = isXml;
+		this.text = ! isXml;
+	}
+
+	private GenericDocumentWrapper (Reader reader, boolean isXml)
+	{
+		this.reader = reader;
+
+		this.xml = isXml;
+		this.text = ! isXml;
+	}
+
+	private GenericDocumentWrapper (InputStream inputStream, boolean isXml)
+	{
+		this.inputStream = inputStream;
+
+		this.xml = isXml;
+		this.text = ! isXml;
+	}
+
+	private GenericDocumentWrapper (InputStream inputStream)
+	{
+		this.inputStream = inputStream;
+
+		this.binary = true;
+	}
+
+	public static XQDocumentWrapper newXml (String xml)
+	{
+		return new GenericDocumentWrapper (xml, true);
+	}
+
+	public static XQDocumentWrapper newText (String text)
+	{
+		return new GenericDocumentWrapper (text, false);
+	}
+
+	public static XQDocumentWrapper newXml (Reader reader)
+	{
+		return new GenericDocumentWrapper (reader, true);
+	}
+
+	public static XQDocumentWrapper newText (Reader reader)
+	{
+		return new GenericDocumentWrapper (reader, false);
+	}
+
+	public static XQDocumentWrapper newXml (InputStream inputStream)
+	{
+		return new GenericDocumentWrapper (inputStream, true);
+	}
+
+	public static XQDocumentWrapper newText (InputStream inputStream)
+	{
+		return new GenericDocumentWrapper (inputStream, false);
+	}
+
+	public static XQDocumentWrapper newBinary (InputStream inputStream)
+	{
+		return new GenericDocumentWrapper (inputStream);
+	}
+
+	// ---------------------------------------------------------------
+
+	public boolean isXml ()
+	{
+		return (xml);
+	}
+
+	public boolean isText ()
+	{
+		return (text);
+	}
+
+	public boolean isBinary ()
+	{
+		return (binary);
+	}
+
+	public InputStream asStream ()
+	{
+		if (body != null) {
+			try {
+				return (new ByteArrayInputStream (body.getBytes ("UTF-8")));
+			} catch (UnsupportedEncodingException e) {
+				throw new RuntimeException ("UTF-8 encoding not available - *should never happen*", e);
+			}
+		}
+
+		if (inputStream != null) {
+			InputStream tmp = inputStream;
+
+			inputStream = null;
+
+			return (tmp);
+		}
+
+		throw new IllegalStateException ("No ready InputStream available");
+	}
+
+	public synchronized Reader asReader ()
+	{
+		if (body != null) {
+			return (new StringReader (body));
+		}
+
+		if (reader != null) {
+			Reader tmp = reader;
+
+			reader = null;
+
+			return (tmp);
+		}
+
+		throw new IllegalStateException ("No ready Reader available");
+	}
+}
